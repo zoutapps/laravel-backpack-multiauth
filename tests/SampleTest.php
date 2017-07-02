0cff2c5fc29f9b17b5d521869e7e24c1c3148ac6
Oliver Ziegler <oliver.ziegler@webfactor.de>
2017-07-02 04:33:22 +0200

    add sample test

---
+++ tests/SampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ZoutApps\LaravelBackpackAuth\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    /** @test */
+    public function sample_test()
+    {
+        $this->assertTrue(true);
+    }
+}
