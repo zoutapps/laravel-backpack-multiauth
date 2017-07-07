5c18887e4e2cef256cc7d7f117d378c72bcabd5b
Oliver Ziegler <oliver.ziegler@zoutapps.de>
2017-07-07 17:50:57 +0200

    Add secure confirm when not using -f

--- src/Commands/AuthCommand.php
+++ src/Commands/AuthCommand.php
@@ -88,6 +88,9 @@ abstract class AuthCommand extends Command
     protected function checkInput(): bool
     {
         if (!$this->option('force')) {
+            if(!$this->confirm('This command will generate new files, overwrite and append existing ones. Do you want to proceed?')) {
+                return false;
+            }
             $this->info('--force omitted. Will ask for overwrite of existing files.');
         }
 
