7d2022c03eafa88b3f61770195bf01565f606c16
Oliver Ziegler <oliver.ziegler@zoutapps.de>
2017-07-25 10:49:35 +0200

    add test for ModelGenerator

---
+++ tests/LaravelTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ZoutApps\LaravelBackpackAuth\Test;
+
+use \Illuminate\Foundation\Testing\TestCase;
+use ZoutApps\LaravelBackpackAuth\AuthServiceProvider;
+
+class LaravelTest extends  TestCase
+{
+
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../vendor/laravel/laravel/bootstrap/app.php';
+
+        $app->register(AuthServiceProvider::class);
+
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+        return $app;
+    }
+}
