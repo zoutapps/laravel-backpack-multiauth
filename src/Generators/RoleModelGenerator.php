234280e8c5ffa69843b34a9bd9711c29900863f0
Oliver Ziegler <oliver.ziegler@zoutapps.de>
2017-07-25 12:53:12 +0200

    fix role model paths

--- src/Generators/RoleModelGenerator.php
+++ src/Generators/RoleModelGenerator.php
@@ -7,9 +7,9 @@ class RoleModelGenerator extends ModelGenerator
     protected function stubPath($lucid = false)
     {
         if ($lucid) {
-            return __DIR__.'/../../stubs/Lucid/Model/RoleModel.stub';
+            return __DIR__.'/../stubs/Lucid/Model/RoleModel.stub';
         } else {
-            return __DIR__.'/../../stubs/Model/RoleModel.stub';
+            return __DIR__.'/../stubs/Model/RoleModel.stub';
         }
     }
 }
